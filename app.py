dba017f13b891cc73af4ee3f0a8bc0f8ad71e8cf
BrayanQuirozUrrutia <brayanquirozurrutia@gmail.com>
2024-08-29 21:05:00 -0400

    :construction: FEAT: Add endpoint to upload csv with ingredients
    Add an endpoint to be able to upload the ingredients in csv format. Added function to compare ingredients extracted from the image and verify if they have any risk, the function returns the risk level of the scanned product.

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import io
+import os
+import logging
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from PIL import Image
+from flask_migrate import Migrate
+import pandas as pd
 import pytesseract
-import io
-import cv2
 import numpy as np
+import cv2
+from PIL import Image
 from dotenv import load_dotenv
-import os
 import google.generativeai as genai
-import logging
+
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -26,11 +29,12 @@ genai.configure(api_key=OPENAI_API_KEY)
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ingredient = db.Column(db.String(255), nullable=False)
-    risk = db.Column(db.String(255), nullable=False)
+    ingredient = db.Column(db.String(255), nullable=False, unique=True)
+    risk = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return f"<Ingredient {self.ingredient}>"
@@ -100,8 +104,115 @@ def extract_ingredients(ingredients: str) -> str:
     return response.text
 
 
+def calculate_risk(ingredients: list[str]) -> dict:
+    """
+    Calculates the risk of a product based on the ingredients and their risks in the database.
+
+    :param ingredients: List of ingredients in the product.
+    :return: A dictionary with the normalized risk, the risk classification, and the list of risky ingredients.
+    """
+    n_total = len(ingredients)
+    total_risks = 0
+    n_risky = 0
+    risky_ingredients = []
+
+    for ingredient in ingredients:
+        ingredient_upper = ingredient.upper().strip()
+        ing = Ingredient.query.filter_by(ingredient=ingredient_upper).first()
+        if ing:
+            total_risks += ing.risk
+            n_risky += 1
+            if ingredient_upper not in risky_ingredients:
+                risky_ingredients.append(ingredient_upper.title())
+
+    if n_total == 0:
+        return {
+            'normalized_risk': 0,
+            'classification': "N/A",
+            'risky_ingredients': []
+        }
+
+    total_risk = total_risks / n_total
+    normalized_risk = total_risk * 100
+
+    if normalized_risk <= 33:
+        classification = "Bajo riesgo"
+    elif normalized_risk <= 66:
+        classification = "Mediano riesgo"
+    else:
+        classification = "Alto riesgo"
+
+    return {
+        'normalized_risk': round(normalized_risk, 2),
+        'classification': classification,
+        'risky_ingredients': risky_ingredients
+    }
+
+
+def normalize_ingredient(ingredient: str) -> str:
+    """
+    Normalize the ingredient name by converting to uppercase and removing extra spaces.
+    """
+    return ' '.join(ingredient.upper().split())
+
+@app.route('/upload_excel', methods=['POST'])
+def upload_excel():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file and file.filename.endswith('.xlsx'):
+        try:
+            df = pd.read_excel(file)
+
+            if 'ingredient' not in df.columns or 'risk' not in df.columns:
+                return jsonify({'error': 'Invalid Excel format'}), 400
+
+            df['ingredient'] = df['ingredient'].apply(normalize_ingredient)
+            existing_ingredients = {i.ingredient: i for i in Ingredient.query.all()}
+
+            new_ingredients = []
+            for index, row in df.iterrows():
+                ingredient = row['ingredient']
+                risk = row['risk']
+
+                if ingredient in existing_ingredients:
+                    existing_ingredients[ingredient].risk = risk
+                else:
+                    new_ingredients.append(Ingredient(ingredient=ingredient, risk=risk))
+
+            if new_ingredients:
+                db.session.add_all(new_ingredients)
+
+            db.session.commit()
+            return jsonify({'message': 'Ingredients uploaded successfully'}), 200
+
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({'error': str(e)}), 500
+
+    return jsonify({'error': 'Invalid file format'}), 400
+
+
 @app.route('/upload', methods=['POST'])
 def upload_image():
+    """
+    Upload an image and extract the ingredients and calculate the risk of the product.
+    :return: JSON response with the risk of the product and the list of risky ingredients.
+    {
+    "classification": "Mediano riesgo",
+    "normalized_risk": 54.29,
+    "risky_ingredients": [
+        "Ácido Palmítico",
+        "Ácido Esteárico",
+        "Ácido Málico",
+        "Ácido Cítrico"
+    ]
+}
+    """
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
 
@@ -118,10 +229,16 @@ def upload_image():
             normalized_text = process_text(text)
             #logging.debug(f'Normalized text: {normalized_text}')
             ingredients = extract_ingredients(normalized_text)
+            risk = calculate_risk(ingredients.split(','))
             #logging.debug(f'Ingredients: {ingredients}')
-            return jsonify({'ingredients': ingredients}), 200
+            return jsonify(
+                risk
+            ), 200
         except Exception as e:
-            return jsonify({'error': str(e)}), 500
+            print(e)
+            return jsonify({
+                'error': 'Ocurrió un error al procesar la imagen'
+            }), 500
 
     return jsonify({'error': 'Invalid file format'}), 400
 
